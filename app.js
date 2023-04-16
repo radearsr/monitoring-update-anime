2c716310654654d815649e541f9fd06ed10ef83c
radearsr <radea.surya4@gmail.com>
2023-04-16 20:06:47 +0700

    adding command bot for action monitoring anime, and episodes

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 require("dotenv").config();
 const { Cron } = require("croner");
+const { Telegraf } = require("telegraf")
 const prismaServices = require("./services/prismaServices");
 const axiosServices = require("./services/axiosServices");
 const utils = require("./utils");
 
 const ADDON_API_ENDPOINT = "http://localhost:4000";
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const monitoringAnimesServices = async () => {
+const monitoringAnimesServices = async (botToken, chatId) => {
   try {
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `Monit Anime Start [${utils.currentTime()}]`);
+    await axiosServices.senderNofitication(botToken, chatId, `Monit Anime Start [${utils.currentTime()}]`);
     const liveListAnimes = await axiosServices.getAllAnimes(ADDON_API_ENDPOINT, "https://otakudesu.lol/anime-list");
     const localCountAnimes = await prismaServices.getCountAnimes();
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `LIVE=${liveListAnimes.length}\nLOCAL=${localCountAnimes}\n[${utils.currentTime()}]`);
+    await axiosServices.senderNofitication(botToken, chatId, `LIVE=${liveListAnimes.length}\nLOCAL=${localCountAnimes}\n[${utils.currentTime()}]`);
     if (liveListAnimes.length <= localCountAnimes) {
-      await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `Anime Up To Date [${utils.currentTime()}]`);
-      return axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `Monit Anime End [${utils.currentTime()}]`);
+      await axiosServices.senderNofitication(botToken, chatId, `Anime Up To Date [${utils.currentTime()}]`);
+      return axiosServices.senderNofitication(botToken, chatId, `Monit Anime End [${utils.currentTime()}]`);
     }
     const localAllAnimes = await prismaServices.getAllAnimes();
     const updatedAnimes = utils.compareAndListed(localAllAnimes, liveListAnimes);
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `Update Anime ${updatedAnimes.length} [${utils.currentTime()}]`);
+    await axiosServices.senderNofitication(botToken, chatId, `Update Anime ${updatedAnimes.length} [${utils.currentTime()}]`);
     const detailWithTrouble = {
       lists: []
     };
@@ -39,16 +41,16 @@ const monitoringAnimesServices = async () => {
     }));
     const filteredUndefinedDetails = updatedWithDetails.filter((updatedList) => updatedList !== undefined);
     console.log(filteredUndefinedDetails);
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `Anime Trouble ${JSON.stringify(detailWithTrouble)} [${utils.currentTime()}]`);
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `Monit Anime End [${utils.currentTime()}]`);
+    await axiosServices.senderNofitication(botToken, chatId, `Anime Trouble ${JSON.stringify(detailWithTrouble)} [${utils.currentTime()}]`);
+    await axiosServices.senderNofitication(botToken, chatId, `Monit Anime End [${utils.currentTime()}]`);
   } catch (error) {
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, error.message);
+    await axiosServices.senderNofitication(botToken, chatId, error.message);
   }
 }
 
-const monitoringEpisodeServices = async () => {
+const monitoringEpisodeServices = async (botToken, chatId) => {
   try {
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `Monit Episode Start [${utils.currentTime()}]`);
+    await axiosServices.senderNofitication(botToken, chatId, `Monit Episode Start [${utils.currentTime()}]`);
     console.log(`Program Start [${utils.currentTime()}]`);
     const updatedAnimes = [];
     // Get Animes Ongoing From Supabase DB
@@ -93,23 +95,29 @@ const monitoringEpisodeServices = async () => {
         }
       }
     }));
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `Jumlah Anime Update ${updatedAnimes.length}`);
+    await axiosServices.senderNofitication(botToken, chatId, `Jumlah Anime Update ${updatedAnimes.length}`);
     console.log(payloadForUpdate);
     payloadForUpdate.forEach(async (update, idx) => {
       setTimeout(async () => {
         await prismaServices.createEpisode(update.payload);
-        await axiosServices.senderSuccessUpdateAnime(process.env.BOT_TOKEN, process.env.GROUP_ID, update.notif.title, update.notif.episode);
+        await axiosServices.senderSuccessUpdateAnime(botToken, chatId, update.notif.title, update.notif.episode);
       }, (idx * 10000))
     });
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, `Monit Episode End [${utils.currentTime()}]`);
+    await axiosServices.senderNofitication(botToken, chatId, `Monit Episode End [${utils.currentTime()}]`);
   } catch (error) {
-    await axiosServices.senderNofitication(process.env.BOT_TOKEN, process.env.GROUP_ID, error.message);
+    await axiosServices.senderNofitication(botToken, chatId, error.message);
   }
 }
 
 Cron("0 */6 * * *", { timezone: "Asia/Jakarta" }, () => {
 });
 
-(async () => {
-  await monitoringAnimesServices()
-})()
+bot.command("newanime", async (ctx) => {
+  await monitoringAnimesServices(process.env.BOT_TOKEN, ctx.chat.id);
+});
+
+bot.command("newepisode", async (ctx) => {
+  await monitoringEpisodeServices(process.env.BOT_TOKEN, ctx.chat.id);
+});
+
+bot.launch();
